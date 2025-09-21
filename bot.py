39bd6ff589a8cd85f65452c6dedb7d047263b181
Jafar Moukalled <jmoukall@gmu.edu>
2025-09-21 17:54:07 -0400

    edited hosts and ports

--- bot.py
+++ bot.py
@@ -45,5 +45,7 @@ def message(payload):
 
     return response, 200
 
+PORT = int(os.environ.get("PORT", 8080))
+
 if __name__ == "__main__":
-    app.run(port=8080)
+    app.run(host="0.0.0.0", port=PORT)
